a511709e17f81ae9d0ba07bd49d808dfe411e6af
jorge <jorgevaldivia02@gmail.com>
2023-05-30 20:37:00 +0200

    WIP: change status of task on firebase

--- app/src/main/java/com/example/taskodoro/recycler_view/TaskMainAdapter.java
+++ app/src/main/java/com/example/taskodoro/recycler_view/TaskMainAdapter.java
@@ -1,18 +1,19 @@
 package com.example.taskodoro.recycler_view;
 
-import android.os.TestLooperManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.taskodoro.R;
-import com.example.taskodoro.classes.Sesion;
 import com.example.taskodoro.classes.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -20,6 +21,10 @@ public class TaskMainAdapter extends RecyclerView.Adapter<TaskMainAdapter.ViewHo
 
     private int resource;
     private ArrayList<Task> tasks;
+
+    private DatabaseReference myRef;
+
+    private FirebaseDatabase database;
 
     public TaskMainAdapter(int resource, ArrayList<Task> tasks) {
         this.resource = resource;
@@ -36,9 +41,17 @@ public class TaskMainAdapter extends RecyclerView.Adapter<TaskMainAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
         Task task = tasks.get(position);
         holder.txt_main_task_name.setText(task.getTaskName());
         holder.cb_main_task_status.setChecked(task.getTaskStatus());
+        holder.cb_main_task_status.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                myRef.child("sesions").child(currentUser).child(sesionName).child("tasks").child(task.getTaskName()).child("taskStatus").setValue(b);
+            }
+        });
     }
 
     @Override
